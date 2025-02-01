a06c1d3d5e2e380dab6fb29cd16cd11eb998ebb1
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-01 21:31:07 +0100

    Add Home, About, Post, AddPost, EditPost and NotFound components, add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/pages/Home/Home.js";
+import About from "./components/pages/About/About.js";
+import Post from "./components/pages/Post/Post.js";
+import AddPost from "./components/pages/AddPost/AddPost.js";
+import EditPost from "./components/pages/EditPost/EditPost.js";
+import NotFound from './components/pages/NotFound/NotFound.js';
+
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/add" element={<AddPost />} />
+        <Route path="/post/edit/:id" element={<EditPost />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
