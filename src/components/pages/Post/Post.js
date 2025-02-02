ba470706204059e09f1722eb9fd104a94a6c9734
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-02 18:32:22 +0100

    Add removePost action creator to postReducer, add ModalPage component, add remove method and update Post component

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,14 +1,19 @@
-import { getPostById } from "../../../redux/postsReducer";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { Col, Row, Button, Container, Card } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { getPostById, removePost } from "../../../redux/postsReducer";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, Navigate, NavLink } from "react-router-dom";
+import { Col, Row, Button, Container } from "react-bootstrap";
+import ModalPage from "../../features/ModalPage/ModalPage";
 
 const Post = () => {
     const { id } = useParams();
+    const dispatch = useDispatch();
     const postData = useSelector(state => getPostById(state, id));
-    console.log(id);
-    return(
+    const remove = e => {
+        e.preventDefault();
+        dispatch(removePost(postData.id));
+    }
+    if(!postData) return <Navigate to="/" />
+    else return(
         <Row className="pt-3">
             <Container className="d-flex justify-content-center">
                 <Col xs={6}>                
@@ -19,7 +24,7 @@ const Post = () => {
                 </Col>
                 <Col xs={2}>
                     <Button variant="outline-info" className="m-1" as={NavLink} to="/post/edit/:id">Edit</Button>
-                    <Button variant="outline-danger" className="m-1">Delete</Button>
+                    <ModalPage action={ remove } buttonName="Delete" content="This action will completely remove this post from the app. Are you sure you want to do this?" />
                 </Col>
             </Container>
         </Row>
--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -4,10 +4,14 @@ export const getPostById = ({posts}, postId) => posts.find(post => post.id === p
 
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName("REMOVE_POST");
 
 // action creators
+export const removePost = payload => ({ type: REMOVE_POST, payload})
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter(post => post.id !== action.payload);
     default:
       return statePart;
   };
