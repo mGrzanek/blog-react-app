5dd87710364bb65fbb6758ab13c1cbba1f22ab9e
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-05 15:57:00 +0100

    Update CategoryLink component

--- src/components/views/CategoryLink/CategoryLink.js
+++ src/components/views/CategoryLink/CategoryLink.js
@@ -1,9 +1,19 @@
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Badge } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { getPostByCategory } from "../../../redux/postsReducer";
+import { useSelector } from "react-redux";
 
 const CategoryLink = ({ id, category }) => {
+    const posts = useSelector(state => getPostByCategory(state, category));
     return(
-        <ListGroup.Item key={id} as={NavLink} to={`/categories/${(category).toLowerCase()}`}>{category}</ListGroup.Item>
+        <ListGroup.Item 
+            className="d-flex justify-content-between align-items-center" 
+            key={id} as={NavLink} 
+            to={`/categories/${(category).toLowerCase()}`}
+        >
+            {category}
+            <Badge bg="primary">{posts.length}</Badge>
+        </ListGroup.Item>
     );
 }
 
